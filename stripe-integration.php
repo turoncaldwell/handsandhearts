84532482831eeff3dcc9b3d583f837a15a6c84c0
turoncaldwell <chieftechieturon@handsandheartsthatcare.org>
2025-05-06 21:35:38 -0400

    Add Stripe integration stub (sanitized)

---
+++ stripe-integration.php
@@ -0,0 +1,7 @@
+<?php
+// Sanitized: Stripe API keys removed
+// require_once 'vendor/autoload.php';
+// \Stripe\Stripe::setApiKey('sk_test_...');
+// $charge = \Stripe\Charge::create(['amount' => 1000, 'currency' => 'usd', 'source' => 'tok_visa']);
+echo "Payment processing stub (Stripe integration removed for security).";
+?>
